caaeea913acbd21cc749872ae0178f55e781e6f8
Pablo del Barrio <pablodbb@gmail.com>
2023-10-17 14:12:55 +0200

    basic structure and routing of main components done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
+import {useNavigation} from '@react-navigation/native';
+import {Button, Icon} from '@rneui/themed';
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 const staticInfo = {
   name: 'Pablo del Barrio',
@@ -7,18 +9,57 @@ const staticInfo = {
 };
 
 const Header = () => {
+  const {canGoBack, goBack} = useNavigation();
+
   return (
     <View style={styles.container}>
-      <View style={styles.leftContainer}></View>
-      <View style={styles.rightContainer}></View>
+      {canGoBack() ? (
+        <View style={styles.arrowContainer}>
+          <Button
+            icon={<Icon name="arrow-back" size={24} />}
+            type="clear"
+            onPress={() => goBack()}
+          />
+        </View>
+      ) : undefined}
+      <View style={styles.leftContainer}>
+        <Text style={styles.name}>{`Hello ${staticInfo.name}`}</Text>
+        <Text style={styles.subtitle}>Welcome back to your goal</Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <Image source={{uri: staticInfo.uri}} style={styles.profileImage} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  leftContainer: {},
-  rightContainer: {},
+  container: {
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  subtitle: {
+    fontSize: 12,
+    color: '#808080',
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 24,
+  },
+  arrowContainer: {},
 });
 
 export default Header;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,14 +1,63 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {Button, Icon} from '@rneui/themed';
 
+import Header from '../../components/Header';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../../types';
 const Home = () => {
+  const {navigate} =
+    useNavigation<StackNavigationProp<RootStackParamList, 'Home'>>();
+
+  const handleAddCaloriesPress = () => {
+    navigate('AddFood');
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.caloriesContainer}>
+        <View style={styles.leftContainer}>
+          <Text style={styles.caloriesLegend}>Calories</Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <Button
+            icon={<Icon name="add-circle-outline" color="#fff" />}
+            radius="lg"
+            color="#4ecb71"
+            onPress={handleAddCaloriesPress}
+          />
+        </View>
+      </View>
       <Text>AddFood</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  leftContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  caloriesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 24,
+  },
+  caloriesLegend: {
+    fontSize: 20,
+  },
+});
 
 export default Home;
